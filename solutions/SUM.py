ba6d66ad0a4a0965a1f3c57581131dadf39a146d
sbx_user1051 <sbx_user1051@169.254.100.141>
2019-07-28 23:22:29 +0000

    Sun Jul 28 23:22:29 UTC 2019

--- solutions/SUM.py
+++ solutions/SUM.py
@@ -8,7 +8,7 @@ def sum(a, b):
      - param[0] = a positive integer between 0-100
      - param[1] = a positive integer between 0-100
      - @return = an Integer representing the sum of the two numbers"""
-    if a not in range(0, 100) or b not in range(0, 100) or type(a) is not int or type(b) is not int:
-        raise ValueError("Values not in range or of required type")
+    # if a not in range(0, 100) or b not in range(0, 100) or type(a) is not int or type(b) is not int:
+    #     raise ValueError("Values not in range or of required type")
 
     return a+b
