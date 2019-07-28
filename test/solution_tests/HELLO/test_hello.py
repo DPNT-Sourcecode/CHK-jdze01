92d794169a9fc3590389289b2f026f6c656d774f
sbx_user1051 <sbx_user1051@169.254.100.141>
2019-07-28 23:58:08 +0000

    Sun Jul 28 23:58:08 UTC 2019

--- test/solution_tests/HELLO/test_hello.py
+++ test/solution_tests/HELLO/test_hello.py
@@ -1,26 +1,20 @@
-from lib.solutions.SUM_R1 import sum
+from lib.solutions.HLO.hello_solution import hello
 import pytest
 
 
-class TestSum:
-    def test_sum_operation(self):
-        assert sum(1, 0) == 1
+class TestHello:
 
-    def test_sum_range(self):
-        with pytest.raises(ValueError) as exceptInfo:
-            sum(102, 2)
-        assert "Values not in range or of required type" in str(exceptInfo.value)
+    def test_hello_input_type(self):
+        with pytest.raises(TypeError) as exceptInfo:
+            hello(2)
+        assert "Input parameter is not of string type" in str(exceptInfo.value)
 
-    def test_sum_range(self):
-        with pytest.raises(ValueError) as exceptInfo:
-            sum(-1, 2)
-        assert "Values not in range or of required type" in str(exceptInfo.value)
 
-    @pytest.mark.parametrize(("a", "b"),
-                             [(1, 2.5),
-                              ('2', 5),
-                              (5, 3+2j)])
-    def test_sum_type(self, a, b):
-        with pytest.raises(ValueError) as exceptInfo:
-            sum(a, b)
-        assert "Values not in range or of required type" in str(exceptInfo.value)
+    # @pytest.mark.parametrize(("a", "b"),
+    #                          [(1, 2.5),
+    #                           ('2', 5),
+    #                           (5, 3+2j)])
+    # def test_sum_type(self, a, b):
+    #     with pytest.raises(ValueError) as exceptInfo:
+    #         sum(a, b)
+    #     assert "Values not in range or of required type" in str(exceptInfo.value)
