f3b99561b19837acac6958c03b8976843081546b
sbx_user1051 <sbx_user1051@169.254.249.85>
2019-07-28 19:52:05 +0000

    Sun Jul 28 19:52:05 UTC 2019

--- solutions/sum.py
+++ solutions/sum.py
@@ -1,5 +1,5 @@
 
-def sum(a: int, b: int):
+def sum_integers(a: int, b: int):
     """In order to complete the round you need to implement the following method:
          sum(Integer, Integer) -> Integer
 
@@ -7,5 +7,9 @@ def sum(a: int, b: int):
      - param[0] = a positive integer between 0-100
      - param[1] = a positive integer between 0-100
      - @return = an Integer representing the sum of the two numbers"""
+    if a not in range(100) or b not in range(100):
+        return None
+
     return a+b
+
 
