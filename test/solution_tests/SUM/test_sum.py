8e9ed77f1e851d8091e22d3f9b9777e9660a39b0
sbx_user1051 <sbx_user1051@169.254.100.141>
2019-07-28 22:53:25 +0000

    Sun Jul 28 22:53:25 UTC 2019

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -10,12 +10,12 @@ class TestSum():
             sum_solution(102, 2)
         assert "Values not in range" in str(exceptInfo.value)
 
-    def test_sum_type(self):
+    @pytest.mark.parametrize(("a", "b"),
+                             [(1, 2.5),
+                              ('2', 5),
+                              (5, 3+2j)])
+    def test_sum_type(self, a, b):
         with pytest.raises(ValueError) as exceptInfo:
             sum_solution('102', 2.5)
         assert "Input values are not of required type" in str(exceptInfo.value)
-
-
-    # elif type(a) is not int or type(b) is not int:
-    #     raise ValueError("")
 
