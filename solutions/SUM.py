e630c3eedcc399083c4120b77c9d0df63953fc4e
sbx_user1051 <sbx_user1051@169.254.100.141>
2019-07-28 22:44:26 +0000

    Sun Jul 28 22:44:26 UTC 2019

--- solutions/SUM.py
+++ solutions/SUM.py
@@ -1,6 +1,6 @@
 
 
-def sum_solution(a: int, b: int):
+def sum_solution(a, b):
     """In order to complete the round you need to implement the following method:
          sum(Integer, Integer) -> Integer
 
@@ -9,6 +9,8 @@ def sum_solution(a: int, b: int):
      - param[1] = a positive integer between 0-100
      - @return = an Integer representing the sum of the two numbers"""
     if a not in range(100) or b not in range(100):
-        return None
+        raise ValueError("Values not in range")
+    elif type(a) is not int or type(b) is not int:
+        raise ValueError("Input values are not of required type")
 
     return a+b
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,9 +1,15 @@
 from solutions.SUM import sum_solution
-
+import pytest
 
 class TestSum():
-    def test_sum(self):
+    def test_sum_operation(self):
         assert sum_solution(1, 2) == 3
 
+    def test_sum_range(self):
+        
 
+    #
+    #     raise ValueError("Values not in range")
+    # elif type(a) is not int or type(b) is not int:
+    #     raise ValueError("Input values are not of required type")
 
