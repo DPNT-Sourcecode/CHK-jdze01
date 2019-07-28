68d03c95e8db509e4364bfea0c01bd2993f1a616
sbx_user1051 <sbx_user1051@169.254.100.141>
2019-07-28 23:41:15 +0000

    Sun Jul 28 23:41:15 UTC 2019

---
+++ lib/solutions/SUM_R1/sum_solution.py
@@ -0,0 +1,13 @@
+
+def sum(a, b):
+    """In order to complete the round you need to implement the following method:
+         sum(Integer, Integer) -> Integer
+
+    Where:
+     - param[0] = a positive integer between 0-100
+     - param[1] = a positive integer between 0-100
+     - @return = an Integer representing the sum of the two numbers"""
+    if a not in range(0, 101) or b not in range(0, 101) or type(a) is not int or type(b) is not int:
+        raise ValueError("Values not in range or of required type")
+
+    return a+b
