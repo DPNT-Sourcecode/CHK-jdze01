ac8a205840124bb45a424588a1bb199290757c22
sbx_user1051 <sbx_user1051@169.254.100.141>
2019-07-28 22:56:25 +0000

    Sun Jul 28 22:56:25 UTC 2019

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,7 @@
 from solutions.SUM import sum_solution
 import pytest
 
+
 class TestSum():
     def test_sum_operation(self):
         assert sum_solution(1, 2) == 3
@@ -16,6 +17,6 @@ class TestSum():
                               (5, 3+2j)])
     def test_sum_type(self, a, b):
         with pytest.raises(ValueError) as exceptInfo:
-            sum_solution('102', 2.5)
+            sum_solution(a, b)
         assert "Input values are not of required type" in str(exceptInfo.value)
 
