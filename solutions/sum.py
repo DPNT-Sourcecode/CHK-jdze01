ef27e79bc04d3c03ac20383eafcb435365501e86
sbx_user1051 <sbx_user1051@169.254.249.85>
2019-07-28 19:46:06 +0000

    Sun Jul 28 19:46:06 UTC 2019

---
+++ solutions/sum.py
@@ -0,0 +1,10 @@
+
+def sum(a, b):
+    """In order to complete the round you need to implement the following method:
+         sum(Integer, Integer) -> Integer
+
+    Where:
+     - param[0] = a positive integer between 0-100
+     - param[1] = a positive integer between 0-100
+     - @return = an Integer representing the sum of the two numbers"""
+    return a+b
