6c56ce61ff8cc02e8b1f542faa7541fbd39cb7f4
sbx_user1051 <sbx_user1051@169.254.249.85>
2019-07-28 19:58:06 +0000

    Sun Jul 28 19:58:06 UTC 2019

---
+++ main_app.py
@@ -0,0 +1,6 @@
+from solutions.SUM_R1 import sum_integers
+
+
+if __name__ == '__main__':
+    print(sum_integers(1, 2))
+
--- maine_app.py
+++
@@ -1,5 +0,0 @@
-from solutions.SUM import sum_integers
-
-print(sum_integers(1,2))
-
-
