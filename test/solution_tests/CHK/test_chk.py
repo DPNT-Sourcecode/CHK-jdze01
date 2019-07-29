4c740b44dc7418f6f50b8248c969fc49e7d9f460
sbx_user1051 <sbx_user1051@169.254.100.141>
2019-07-29 01:19:06 +0000

    Mon Jul 29 01:19:06 UTC 2019

---
+++ test/solution_tests/CHK/test_chk.py
@@ -0,0 +1,28 @@
+from lib.solutions.HLO.hello_solution import hello
+import pytest
+
+
+class TestHello:
+
+    def test_hello_input_type(self):
+        with pytest.raises(TypeError) as exceptInfo:
+            hello(2)
+        assert "Input parameter is not of string type" in str(exceptInfo.value)
+
+    # def test_hello_output_type(self):
+    #     with pytest.raises(TypeError) as exceptInfo:
+    #        message = hello("name")
+    #     assert "Output message is not of string type" in str(exceptInfo.value)
+
+    def test_hello_output_content(self):
+        msg = hello("John")
+        assert msg == "Hello, John!"
+
+    # @pytest.mark.parametrize(("a", "b"),
+    #                          [(1, 2.5),
+    #                           ('2', 5),
+    #                           (5, 3+2j)])
+    # def test_sum_type(self, a, b):
+    #     with pytest.raises(ValueError) as exceptInfo:
+    #         sum(a, b)
+    #     assert "Values not in range or of required type" in str(exceptInfo.value)
