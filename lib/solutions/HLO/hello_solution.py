56eb3c9f583ea81f36fbf89a11e4cc1d1a6314c8
sbx_user1051 <sbx_user1051@169.254.100.141>
2019-07-29 00:05:43 +0000

    Mon Jul 29 00:05:43 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,11 +3,11 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    #raise NotImplementedError()
     if type(friend_name) is not str:
         raise TypeError("Input parameter is not of string type")
-    message = str(2)
+    message = 2
     if type(message) is not str:
         raise TypeError("Output message is not of string type")
     return message
+
 
