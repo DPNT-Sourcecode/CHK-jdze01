b290b70f78c3bf00a1d693185504b85e636e3b2d
sbx_user1051 <sbx_user1051@169.254.100.141>
2019-07-29 00:58:06 +0000

    Mon Jul 29 00:58:06 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,6 +4,8 @@ In our store, we'll use individual letters of the alphabet (A, B, C, and so on).
 Our goods are priced individually. In addition, some items are multi-priced: buy n of them, and they'll cost you y pounds.
 For example, item A might cost 50 pounds individually, but this week we have a special offer:
  buy three As and they'll cost you 130."""
+    def get_digits(string):
+        return int("".join([i for i in string if i.isdigit()]))
     skus = skus.split()
     total_sum = 0
     store_skus = {"A": 50, "B": 30, "C": 20, "D": 15}
@@ -18,7 +20,9 @@ For example, item A might cost 50 pounds individually, but this week we have a s
         if sku is not store_skus:
             total_sum = -1
             break
-        if sku[0].is
+        if sku[0].isdigit():
+
+
     return -1
 
 
@@ -32,5 +36,3 @@ Our price table and offers:
 | C    | 20    |                |
 | D    | 15    |                |
 +------+-------+----------------+"""
-
-
