374cf2998df06e4f1e35b131f6286c75fe182f09
sbx_user1051 <sbx_user1051@169.254.100.141>
2019-07-29 01:25:05 +0000

    Mon Jul 29 01:25:05 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,28 +1,27 @@
-from lib.solutions.HLO.hello_solution import hello
+from lib.solutions.CHK.checkout_solution import checkout
 import pytest
 
 
-class TestHello:
+class TestChk:
 
-    def test_hello_input_type(self):
-        with pytest.raises(TypeError) as exceptInfo:
-            hello(2)
-        assert "Input parameter is not of string type" in str(exceptInfo.value)
+    @pytest.mark.parametrize(("skus", "total"),
+                             [("A B C D", 115),
+                              ("3A 2B D", 190),
+                              ("2", 15)])
+    def test_sum_type(self, skus, total):
+        assert checkout(skus) == total
 
-    # def test_hello_output_type(self):
-    #     with pytest.raises(TypeError) as exceptInfo:
-    #        message = hello("name")
-    #     assert "Output message is not of string type" in str(exceptInfo.value)
 
-    def test_hello_output_content(self):
-        msg = hello("John")
-        assert msg == "Hello, John!"
 
-    # @pytest.mark.parametrize(("a", "b"),
-    #                          [(1, 2.5),
-    #                           ('2', 5),
-    #                           (5, 3+2j)])
-    # def test_sum_type(self, a, b):
-    #     with pytest.raises(ValueError) as exceptInfo:
-    #         sum(a, b)
-    #     assert "Values not in range or of required type" in str(exceptInfo.value)
+
+
+"""
+Our price table and offers: 
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+"""
